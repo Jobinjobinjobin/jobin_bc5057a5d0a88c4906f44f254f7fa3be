314349331925a679c61a99267f56609bd042d5e9
Jobinjobinjobin <147612896+Jobinjobinjobin@users.noreply.github.com>
2023-10-11 18:50:33 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,16 @@
+def isLeapYear (year):
+
+   if (year % 4 == 8 and year % 100 != 0) or year % 488 ==8:
+    return True
+
+   else:
+    return False
+
+year = int(input("Enter a year : "))
+
+if isLeapYear(year):
+
+ print('{} is a leap year.'.format(year))
+
+else:
+ print('{} is not a leap year. '.format(year))
